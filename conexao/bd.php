564b68290fdb47c22f1c1b430416ac7cb477d716
Gabriel Nitta <gnitta75@gmail.com>
2022-12-03 00:44:16 -0300

    att

--- conexao/bd.php
+++ conexao/bd.php
@@ -16,10 +16,28 @@ class MigrationDatabase
                     USER VARCHAR(45) NOT NULL,
                     EMAIL VARCHAR(45) NOT NULL,
                     PASSWORD VARCHAR(45) NOT NULL,
-                    PRIMARY KEY (UUID));");
+                    PRIMARY KEY (ID));");
+        } catch (PDOException $e) {
+            var_dump($e);
+        }
+
+    }
+
+    public function createTableProduct()
+    {
+        try {
+            $this->db->exec("CREATE  TABLE IF NOT EXISTS PRODUTO (
+                    ID INT NOT NULL AUTO_INCREMENT ,
+                    PRODUTO VARCHAR(45) NOT NULL,
+                    DESCRICAO VARCHAR(45) NOT NULL,
+                    PRECO_UNITARIO VARCHAR(45) NOT NULL,
+                    QTD_PRODUTO VARCHAR(45) NOT NULL,
+                    PRIMARY KEY (ID));");
         } catch (PDOException $e) {
             var_dump($e);
         }
     }
 }
+
+
 
--- conexao/connection.php
+++ conexao/connection.php
@@ -1,11 +1,4 @@
 <?php
-
-class Database {
-    public function getDatabase() {
-        $env = parse_ini_file('.env');
-        $connection = mysqli_connect('localhost:8000', 'SYSDBA', 'msaterkey');
-        $db = new PDO($connection);
-        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-        return $db;
-    }
-}
+$username = 'SYSDBA';
+$password = 'masterkey';
+$connection = new PDO( 'mysql:host=localhost;dbname=crud', $username, $password );
