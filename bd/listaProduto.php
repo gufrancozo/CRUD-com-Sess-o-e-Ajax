f5fcf1cdcae0488f0e3e2311ab3f0b95cbe288e7
Gabriel Nitta <gnitta75@gmail.com>
2022-12-02 22:30:00 -0300

    Adicionando comandos SQL

--- bd/listaProduto.php
+++ bd/listaProduto.php
@@ -14,11 +14,6 @@ if(isset($_POST['submit'])) {
     $instance = DBConnection::getInstance();
     $conn = $instance->getConnection();
 
-    $nome = $_POST['nome'];
-    $descricao = $_POST['descricao'];
-    $qtd_prdoduto = $_POST['qtd_produto'];
-    $preco_unitario = $_POST['preco_unitario'];
-
 
     $sql = 'SELECT * FROM PRODUTO';
 
